efe0a8c722c82e60cf80d5b9d1baaa0ba1fa5e2f
lucasherlon <lucasherlondsmc@gmail.com>
2023-10-13 10:50:54 -0300

    desafio projeto hospedagem

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 // Cria uma nova reserva, passando a suíte e os hóspedes
 Reserva reserva = new Reserva(diasReservados: 5);
 reserva.CadastrarSuite(suite);
+reserva.Hospedes = new List<Pessoa>();
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
